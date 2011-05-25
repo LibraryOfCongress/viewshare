8c827df47cb5cf72d17fd0ff5658830005451851
David Feeney <dfeeney@gmail.com>
2011-05-25 03:19:48 -0400

    adding mixer.py

---
+++ recollection/mixer.py
@@ -0,0 +1,44 @@
+"""
+RESTful endpoint to receive mixer metadata and initiate profile
+creation based on Akara output.
+"""
+
+from django.contrib.auth.decorators import login_required
+from django.contrib.csrf.middleware import csrf_exempt
+from django.core.urlresolvers import reverse
+from django.http import *
+
+from freemix.dataprofile.models import create_dataset
+from freemix.transform import conf as transform_conf
+from freemix.transform.views import AkaraTransformClient
+
+import json
+from . import conf
+from django.conf import settings
+from urlparse import urljoin
+
+AKARA_MIX_URL = getattr(settings, "AKARA_MIX_URL",
+                        urljoin(transform_conf.AKARA_URL_PREFIX,
+                                "mix.freemix.json"))
+
+mixer = AkaraTransformClient(AKARA_MIX_URL)
+akara = AkaraTransformClient(transform_conf.AKARA_TRANSFORM_URL)
+
+def extract_content(request):
+    return request.POST["mixdata"]
+
+def create_mixed_dataset(use, contents):
+    set = create_dataset(use, contents)
+    url = reverse("edit_data_set",
+                  args=[use.username, set.slug])
+    return HttpResponseRedirect(url)
+
+@csrf_exempt
+@login_required
+def mix_data(request):
+    if request.method == "POST":
+        data = mixer(body=extract_content(request))
+        profile = akara(body=json.dumps(data))
+        return create_mixed_dataset(request.user, profile)
+    else:
+        return HttpResponseNotAllowed(['POST'])
