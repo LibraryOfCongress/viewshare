29b2fd2713e927d4f031fc1d351f19c4b3afe8a7
David Feeney <dfeeney@gmail.com>
2011-06-11 19:06:10 -0400

    adding pretty print function for usernames

--- recollection/apps/profiles/models.py
+++ recollection/apps/profiles/models.py
@@ -7,20 +7,20 @@ from django.utils.translation import ugettext_lazy as _
 from timezones.fields import TimeZoneField
 
 class Profile(models.Model):
-    
+
     user = models.ForeignKey(User, unique=True, verbose_name=_('user'))
     name = models.CharField(_('name'), max_length=50, null=True, blank=True)
     about = models.TextField(_('about'), null=True, blank=True)
     location = models.CharField(_('location'), max_length=40, null=True, blank=True)
     website = models.URLField(_('website'), null=True, blank=True, verify_exists=False)
-    
+
     def __unicode__(self):
         return self.user.username
-    
+
     def get_absolute_url(self):
         return ('profile_detail', None, {'username': self.user.username})
     get_absolute_url = models.permalink(get_absolute_url)
-    
+
     class Meta:
         verbose_name = _('profile')
         verbose_name_plural = _('profiles')
@@ -31,3 +31,12 @@ def create_profile(sender, instance=None, **kwargs):
     profile, created = Profile.objects.get_or_create(user=instance)
 
 post_save.connect(create_profile, sender=User)
+
+def nice_username(u):
+    if u.get_profile().name:
+        return u.get_profile().name
+    else:
+        return u.username
+
+
+
