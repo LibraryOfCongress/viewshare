02a28c2f3deb48825cf14e54a31b2a6c07a53b0b
Mark Baker <mark@zepheira.com>
2011-06-12 11:10:49 -0400

    Initial stab at adding UserVoice tab on every page, with SSO.
    Requires settings.USERVOICE_API_KEY if you want SSO. UV Tab
    is disabled without it.

--- recollection/apps/profiles/urls.py
+++ recollection/apps/profiles/urls.py
@@ -4,5 +4,6 @@ urlpatterns = patterns('',
     url(r'^username_autocomplete/$', 'autocomplete_app.views.username_autocomplete_friends', name='profile_username_autocomplete'),
     url(r'^$', 'recollection.apps.profiles.views.profiles', name='profile_list'),
     url(r'^profile/(?P<username>[\w\._-]+)/$', 'recollection.apps.profiles.views.profile', name='profile_detail'),
+    url(r'^uservoice_token/$', 'recollection.apps.profiles.views.uservoice_token'),
     url(r'^edit/$', 'recollection.apps.profiles.views.profile_edit', name='profile_edit'),
 )
