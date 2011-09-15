d2f02a81dfa451b80093024bd8c7b1a21f66c757
David Feeney <dfeeney@gmail.com>
2011-09-15 12:54:24 -0400

    moved the profile_item.html  template to profiles/profile_item.html and adds STATIC_URL to the template tag context

--- recollection/apps/profiles/templatetags/profile_tags.py
+++ recollection/apps/profiles/templatetags/profile_tags.py
@@ -1,10 +1,11 @@
 from django import template
+from django.conf import settings
 
 register = template.Library()
 
 def show_profile(context, user):
-    return {"user": user, "request": context["request"]}
-register.inclusion_tag("profile_item.html", takes_context=True)(show_profile)
+    return {"user": user, "request": context["request"], "STATIC_URL": settings.STATIC_URL}
+register.inclusion_tag("profiles/profile_item.html", takes_context=True)(show_profile)
 
 def clear_search_url(request):
     getvars = request.GET.copy()
