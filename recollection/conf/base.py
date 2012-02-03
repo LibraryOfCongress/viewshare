c77ab956333eae92022a412b1fc3f39ace458fa6
David Feeney <dfeeney@gmail.com>
2012-02-02 20:28:15 -0500

    restoring SOUTH_MIGRATION_MODULES setting

--- recollection/conf/base.py
+++ recollection/conf/base.py
@@ -2,6 +2,7 @@ from os import path
 from posixpath import join as url_join
 from imp import find_module
 
+
 module_path = lambda m: path.abspath(find_module(m)[1])
 
 MEDIA_ROOT = path.join(module_path("recollection"), "site_media", "media")
