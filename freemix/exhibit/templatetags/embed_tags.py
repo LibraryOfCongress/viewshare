6e54688c838109dfbd2a08a5b72bfb7faf944554
David Feeney <dfeeney@gmail.com>
2012-03-23 01:56:31 -0400

    functioning embedded piechart widget

--- freemix/exhibit/templatetags/embed_tags.py
+++ freemix/exhibit/templatetags/embed_tags.py
@@ -59,45 +59,10 @@ class LabCssCompressor(CssCompressor):
                            mode=mode, context=final_context)
         return render_to_string("exhibit/embed/%s_%s.html" %
                                 (self.type, mode), final_context)
-#    def split_contents(self):
-#        if self.split_content:
-#            return self.split_content
-#        self.media_nodes = []
-#        for elem in self.parser.css_elems():
-#            data = None
-#            elem_name = self.parser.elem_name(elem)
-#            elem_attribs = self.parser.elem_attribs(elem)
-#            if elem_name == 'link' and elem_attribs['rel'].lower() == 'stylesheet':
-#                basename = self.get_basename(elem_attribs['href'])
-#                filename = self.get_filename(basename)
-#                data = (SOURCE_FILE, filename, basename, elem)
-#            elif elem_name == 'style':
-#                data = (SOURCE_HUNK, self.parser.elem_content(elem), None, elem)
-#            if data:
-#                self.split_content.append(data)
-#                media = elem_attribs.get('media', None)
-#                # Append to the previous node if it had the same media type,
-#                # otherwise create a new node.
-#                if self.media_nodes and self.media_nodes[-1][0] == media:
-#                    self.media_nodes[-1][1].split_content.append(data)
-#                else:
-#                    node = LabCssCompressor(content=self.parser.elem_str(elem),
-#                                         context=self.context)
-#                    node.split_content.append(data)
-#                    self.media_nodes.append((media, node))
-#        return self.split_content
+
 
     def output(self, *args, **kwargs):
-        if (settings.COMPRESS_ENABLED or settings.COMPRESS_PRECOMPILERS or
-                kwargs.get('forced', False)):
-            # Populate self.split_content
-            self.split_contents()
-            if hasattr(self, 'media_nodes'):
-                ret = []
-                for media, subnode in self.media_nodes:
-                    subnode.extra_context.update({'media': media})
-                    ret.append(subnode.output(*args, **kwargs))
-#                return ''.join(ret)
+        # Revert to the base output method
         return super(CssCompressor, self).output(*args, **kwargs)
 
 
