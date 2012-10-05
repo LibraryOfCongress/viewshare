f84fe293aa169003f013c3651801f5fb29a3182c
wamberg <wamberg@technushi.com>
2012-10-05 16:27:25 -0400

    Create viewshare.discover.views.front_page to render the home page.

--- viewshare/discover/views.py
+++ viewshare/discover/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.shortcuts import render
+
+from viewshare.discover.models import CuratedExhibit
+
+def front_page(request):
+    """
+    Render the home page.
+    """
+    featured_exhibits = CuratedExhibit.objects.live('front_page')
+    return render(
+            request,
+            'recollection/templates/front_page.html',
+            {'featured_exhibits': featured_exhibits}
+            )
