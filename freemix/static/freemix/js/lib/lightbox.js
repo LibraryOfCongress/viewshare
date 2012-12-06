d5ef85a5b154949838980222ab8d82a1b803c060
David Feeney <dfeeney@gmail.com>
2012-12-06 13:15:00 -0500

    resolves #2138
    Fixed the styling for the custom lightbox displayed for embedded exhibits so it appears overlayed over the exhibit.  Also made the following changes in support of this:
     * Made the lightbox implementations pluggable.  Currently there are two: The default with no external dependencies and the jquery UI dialog used on the main site.  These are namespaced under Lightbox.handlers.{default, jqueryui}.  It's possible for a third party to implement their own dialog by adding a handler to Lightbox.handlers that just takes an Image object, and explicitly setting Lightbox.handler to the appropriate key in Lightbox.handlers.
     * Freemix/exhibit's jquery is appropriately namespaced in embedded views, and any existing jquery (or $ override) is restored after everything is loaded.
     * If jQuery UI is loaded on the embedding page, it will default to the jQuery UI lightbox implementation.  This was actually the case previously, but it was inconsistent.

--- freemix/static/freemix/js/lib/lightbox.js
+++ freemix/static/freemix/js/lib/lightbox.js
@@ -13,30 +13,70 @@ var Lightbox = {
         width: 900,
         height: 550
     },
-    adjusted: { }
+    adjusted: { },
+    handlers: { }
 };
-
-Lightbox.onload = function(src, img) {
-    Lightbox.adjusted.width = Lightbox.defaults.width > $(window).width()-40 ? $(window).width()-40 : Lightbox.defaults.width;
-    Lightbox.adjusted.height = Lightbox.defaults.height > $(window).height()-40 ? $(window).height()-40 : Lightbox.defaults.height;
-    if (typeof $.ui !== "undefined") {
-        $('<p class="center"><img src="' + src + '" /></p>').dialog({
-            modal: true,
-            closeOnEscape: true,
-            dialogClass: 'freemix-themeable',
-            maxWidth: $(window).width() - 50,
-            maxHeight: $(window).height() - 50,
-            width: img.width + 40 > Lightbox.adjusted.width ? Lightbox.adjusted.width : img.width + 40,
-            height: img.height + 65 > Lightbox.adjusted.height ? Lightbox.adjusted.height : img.height + 65,
-            close: function(event, ui) {
-                $(this).dialog('destroy');
-                $(this).remove();
+(function($) {
+    function show_image(img) {
+        if (!Lightbox.handler) {
+            if ((window.jQuery && window.jQuery.ui && window.jQuery.ui.dialog) || ($.ui && $.ui.dialog)) {
+                Lightbox.handler = "jqueryui";
+            } else {
+                Lightbox.handler = "default";
             }
-        });
-    } else {
-        var imgHtml = $('<p class="embed-lightbox-image"><img src="' + src + '" /></p>').css('maxWidth', $(window).width() - 50).css('maxHeight', $(window).height() - 80);
+        }
+        Lightbox.handlers[Lightbox.handler](img);
+    };
+
+    Lightbox.handlers.jqueryui = function(img) {
+        if (!this._$) {
+            if ($.ui && $.ui.dialog) {
+                this._$ = $;
+            } else {
+                this._$ = window.jQuery;
+            }
+        }
+
+        (function($) {
+            Lightbox.adjusted.width = Lightbox.defaults.width > $(window).width()-40 ? $(window).width()-40 : Lightbox.defaults.width;
+            Lightbox.adjusted.height = Lightbox.defaults.height > $(window).height()-40 ? $(window).height()-40 : Lightbox.defaults.height;
+            $('<p class="center"><img src="' + img.src + '" /></p>').dialog({
+                modal: true,
+                closeOnEscape: true,
+                dialogClass: 'freemix-themeable',
+                maxWidth: $(window).width() - 50,
+                maxHeight: $(window).height() - 50,
+                width: img.width + 40 > Lightbox.adjusted.width ? Lightbox.adjusted.width : img.width + 40,
+                height: img.height + 65 > Lightbox.adjusted.height ? Lightbox.adjusted.height : img.height + 65,
+                close: function(event, ui) {
+                    $(this).dialog('destroy');
+                    $(this).remove();
+                }
+            });
+        })(this._$);
+    };
+
+    var zIndex = 0;
+    Lightbox.handlers.default = function(img) {
+        if (zIndex == 0) {
+            zIndex = Math.max.apply(null,$.map($('body > *'), function(e,n){
+                   if($(e).css('position')=='absolute')
+                        return parseInt($(e).css('z-index'))||1 ;
+                   })
+            ) + 1;
+        }
+        var imgHtml = $('<p class="embed-lightbox-image"><img src="' + img.src + '" /></p>').css('maxWidth', $(window).width() - 50).css('maxHeight', $(window).height() - 80);
         var dialogHtml = $('<div class="freemix-themeable embed-lightbox"></div>');
-        var backdropHtml = $('<div>&#160;</div>').css('opacity',0.25).css('backgroundColor','#000').css('zIndex',50000).css('position', 'absolute').css('left', 0).css('top', 0).height($(document).height()).width($(document).width());
+        var backdropHtml = $('<div class="embed-lightbox-backdrop">&#160;</div>')
+            .css('opacity',0.25)
+            .css('backgroundColor','#000')
+            .css('zIndex',zIndex)
+            .css('position', 'absolute')
+            .css('left', "0")
+            .css('top', "0")
+            .height($(document).height())
+            .width("100%");
+
         var close = function(e) {
             if (e.which === 27 && e.type === 'keypress'
                 || e.type !== 'keypress') {
@@ -48,24 +88,38 @@ Lightbox.onload = function(src, img) {
         };
         $(document).bind('keyup', close);
         var closeHtml = $('<p class="embed-lightbox-control"><a href="#">close</a></p>').bind('click', close);
-        backdropHtml.appendTo('body');
+        backdropHtml.appendTo('body').bind('click', close);
         dialogHtml.append(imgHtml);
         dialogHtml.prepend(closeHtml);
         dialogHtml.appendTo('body');
-        w = dialogHtml.outerWidth();
-        h = dialogHtml.outerHeight();
-        dialogHtml.css('left', ($(window).width() - w) / 2).css('top', $(document).scrollTop() + ($(window).height() - h) / 2).css('visibility', 'visible');
-    }
-}
 
-$(document).ready(function() {
-    $('a.lightbox').live('click', function(e) {
-        var src = this.href;
-        var img = new Image();
-        img.onload = function() {
-            Lightbox.onload(src, img);
-        };	
-        img.src = src;
-        e.preventDefault();
+        dialogHtml
+            .css('visibility', 'visible')
+            .css('position', 'absolute')
+            .css('zIndex', zIndex+1);
+
+        var h = dialogHtml.outerHeight();
+        if (h < img.height) {
+            h += img.height;
+        }
+        dialogHtml.css('top', Math.floor($(document).scrollTop() + ($(window).height() - h) / 2));
+
+        dialogHtml.css('left', "50%");
+        var w = dialogHtml.outerWidth();
+        if (w < img.width) {
+            w += img.width;
+        }
+        dialogHtml.css('margin-left', Math.floor(w / 2 * -1));
+    };
+
+    $(document).ready(function() {
+        $(document).on('click', 'a.lightbox', function(e) {
+            var img = new Image();
+            img.onload = function() {
+                show_image(img);
+            };
+            img.src = this.href;
+            e.preventDefault();
+        });
     });
-});
+})(window.Freemix.jQuery);
