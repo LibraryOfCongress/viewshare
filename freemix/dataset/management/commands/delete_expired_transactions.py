a4d7fdcc67cf3dd92e3e4c340fd4a38d430bb1aa
wamberg <wamberg@technushi.com>
2013-02-05 10:29:52 -0500

    Add delete_expired_transactions management command to freemix.dataset to delete expired freemix.dataset.models.DataSourceTransaction records.

---
+++ freemix/dataset/management/commands/delete_expired_transactions.py
@@ -0,0 +1,20 @@
+import logging
+
+from django.core.management.base import NoArgsCommand
+
+from freemix.dataset.models import DataSourceTransaction
+
+
+class Command(NoArgsCommand):
+    help = "Emit queued notices."
+    
+    def handle_noargs(self, **options):
+        logging.basicConfig(level=logging.DEBUG, format="%(message)s")
+        logging.debug("Deleting expired "
+                "freemix.dataset.models.DataSourceTransaction models")
+        transactions = DataSourceTransaction.objects.all()
+        expired_transactions = [x for x in transactions if x.is_expired()]
+        record_count = len(expired_transactions)
+        for expired in expired_transactions:
+            expired.delete()
+        logging.debug("Deleted %s DataSourceTransaction records" % record_count)
