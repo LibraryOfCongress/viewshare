a3c02841980c1a4747e888986009f8218e570ac8
wamberg <wamberg@technushi.com>
2013-03-01 15:37:09 -0500

    flake8 delete_expired_transactions command.

--- freemix/dataset/management/commands/delete_expired_transactions.py
+++ freemix/dataset/management/commands/delete_expired_transactions.py
@@ -8,14 +8,15 @@ from freemix.dataset.models import DataSourceTransaction
 class Command(NoArgsCommand):
     help = ("Delete DataSourceTransaction records that have a modified "
             " date older than the expiration time")
-    
+
     def handle_noargs(self, **options):
         logging.basicConfig(level=logging.DEBUG, format="%(message)s")
         logging.debug("Deleting expired "
-                "freemix.dataset.models.DataSourceTransaction models")
+                      "freemix.dataset.models.DataSourceTransaction models")
         transactions = DataSourceTransaction.objects.all()
         expired_transactions = [x for x in transactions if x.is_expired()]
         record_count = len(expired_transactions)
         for expired in expired_transactions:
             expired.delete()
-        logging.debug("Deleted %s DataSourceTransaction records" % record_count)
+        logging.debug(
+            "Deleted %s DataSourceTransaction records" % record_count)
