698af64a76f7363d857aea7758f993716cff85ca
David Feeney <dfeeney@gmail.com>
2013-04-17 02:54:06 -0400

    reorganize viewshare/static/scripts into app specific directories (with viewshare/static/viewshare/js as a catch-all)

--- freemix/static/freemix/js/exhibit.js
+++ freemix/static/freemix/js/exhibit.js
@@ -64,6 +64,19 @@ Exhibit.Formatter._ImageFormatter.prototype.format = function(value, appender) {
     appender(a);
 };
 
+(function($) {
+    $("body").live('rendered.exhibit', function() {
+        var extype = window.exhibit._database._types['Item'];
+        extype._custom.label = "";
+        extype._custom.pluralLabel = "";
+        $('.exhibit-collectionSummaryWidget-count').parent().contents().filter(function(){
+            return this.nodeType===3;
+        }).remove();
+
+    });
+})(jQuery);
+
+
 (function($, Freemix) {
 
     $.fn.createExhibit = function() {
