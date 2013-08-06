24fc36886be44024819ae7af9d66ddbcc8b13af6
Bill Amberg <wamberg@technushi.com>
2013-08-06 22:00:45 +0000

    Refactor Property model for updated DraftExhibit API.

--- freemix/static/dataset/js/models/editor/property.js
+++ freemix/static/dataset/js/models/editor/property.js
@@ -1,15 +1,10 @@
 /*global define */
 define(
   [
-    'freemix',
     'jquery',
     'observer',
-    'freemix.exhibit',
-    'freemix.property',
-    'freemix.identify',
   ],
   function (
-    Freemix,
     $,
     Observer
   ) {
@@ -19,8 +14,8 @@ define(
    * Represents the name, type, and value of a single attribute of a
    * single item in a DataSource
    * @constructor
-   * @param {string} options.id - ID of the property as identified by Freemix
-   * @param {string} options.name - Name of the property
+   * @param {string} options.id - ID of the property
+   * @param {string} options.label - Label for display purposes
    * @param {string} options.type - Type of the property.
    * Current types include:
    * * text
@@ -29,7 +24,7 @@ define(
    * * date/time
    * * location
    * * number
-   * @param {string} options.value - Value of the property
+   * @param {array} options.items - Array of data values
    */
   var
     PropertyModel = function(options) {
@@ -40,26 +35,16 @@ define(
   $.extend(PropertyModel.prototype, PropertyModelObserver, {
     initialize: function(options) {
       this.id = options.id;
-      this._name = options.name;
+      this._label = options.label;
       this._type = options.type;
-      this.value = options.value;
-      if (Freemix.property.propertyList && Freemix.property.propertyList.hasOwnProperty(this.id)) {
-        // this is an existing Property
-        this.freemixProperty = Freemix.property.propertyList[this.id];
-      } else {
-        // this is a new property
-        this.freemixProperty = undefined;
-      }
+      this.items = options.items;
     },
 
-    /** getter/setter method for name */
-    name: function(newName) {
-      if (newName) {
-        this._name = newName;
-        if (this.freemixProperty) {
-          // This is not a new Property so we can modify it in Freemix
-          this.freemixProperty.label(this._name);
-        }
+    /** getter/setter method for label */
+    label: function(newLabel) {
+      if (newLabel) {
+        this._label = newLabel;
+        // TODO: post data to server
       } else {
         return this._name;
       }
@@ -68,45 +53,14 @@ define(
     /**
      * getter/setter method for type
      * @param {string} newType - Type value for this
-     * @param {bool} options.silent - Flag for triggering changeType event
      */
-    type: function(newType, options) {
-      var silent;
-      if (newType) {
-        silent = (options && options.silent) || false;
-        this._type = newType;
-        if (this.freemixProperty) {
-          // This is not a new Property so we can modify it in Freemix
-          this.freemixProperty.type(this._type);
-        }
-        // publish event to signal other RecordModels should change the type
-        // for this property
-        if (!silent) {
-          this.Observer('changeType').publish({
-            name: this._name,
-            type: this._type
-          });
-        }
+    type: function(newtype) {
+      if (newtype) {
+        this._type = newtype;
+        // TODO: post data to server
       } else {
-        return this._type;
+        return this._name;
       }
-    },
-
-    /** Generate an array used to identify tags in Freemix */
-    tags: function() {
-      return [];
-    },
-
-    /** Create a Freemix Property with our Model's data */
-    createFreemixProperty: function() {
-      var freemixProperty = {
-        property: this._name,
-        label: this._name,
-        enabled: true,
-        tags: this.tags(),
-        types: [this._type]
-      };
-      return freemixProperty;
     },
 
     /**
