d8b2eaf69b5e7114c33799e45b6b3e959697b01d
David Feeney <dfeeney@gmail.com>
2013-08-23 12:45:57 -0400

    move datasource template to upload

--- viewshare/apps/upload/templatetags/datasource_tags.py
+++ viewshare/apps/upload/templatetags/datasource_tags.py
@@ -3,20 +3,7 @@ from viewshare.apps.upload.views import DataSourceRegistry
 
 register = template.Library()
 
-
-@register.inclusion_tag("dataset/datasource_list.html", takes_context=True)
-def datasource_list(context, sources, max_count=10, pageable=True):
-    return {
-            "object_list": sources,
-            "max_count": max_count,
-            "pageable": pageable,
-            "request": context['request'],
-            "sort": context["request"].GET.get("sort", None),
-            "dir": context["request"].GET.get("dir", "desc"),
-            }
-
-
-@register.inclusion_tag("dataset/datasource_list_item.html", takes_context=True)
+@register.inclusion_tag("upload/datasource_list_item.html", takes_context=True)
 def datasource_list_item(context, datasource):
     request = context["request"]
     user = request.user
