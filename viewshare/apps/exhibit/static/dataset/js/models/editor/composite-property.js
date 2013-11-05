347e00a145fe25652192bcae1af07e4e8d756b02
Bill Amberg <bamb@loc.gov>
2013-11-05 12:26:50 -0500

    Add some error handling to status checks that happen before an AugmentTransaction is stored in the database for the property editor.

--- viewshare/apps/exhibit/static/dataset/js/models/editor/composite-property.js
+++ viewshare/apps/exhibit/static/dataset/js/models/editor/composite-property.js
@@ -119,11 +119,25 @@ define(
             }
         },
 
-        /** Failed to check augmentation status on the server */
+        /**
+         * Failed to check augmentation status on the server. This could
+         * happen several ways. One way is that the transformation server
+         * could be unresponsive. Another way is that the augmentation
+         * status check happened before the AugmentTransaction was saved
+         * to the database. In case of the latter, we check the server
+         * this.statusFailureCountdown times.
+         */
         augmentStatusFailure: function(jqxhr, textStatus, error) {
-            this.Observer('augmentDataFailure').publish(
-                {status: textStatus, error: error}
-            );
+            if (this.statusFailureCountdown === 1) {
+                this.Observer('augmentDataFailure').publish(
+                    {status: textStatus, error: error});
+            } else if (this.statusFailureCountdown === undefined) {
+                this.statusFailureCountdown = 2;
+                setTimeout(this.augmentDataSuccess.bind(this), 5000);
+            } else {
+                this.statusFailureCountdown -= 1;
+                setTimeout(this.augmentDataSuccess.bind(this), 5000);
+            }
         },
 
         /** Failed while sending property attributes to the server */
