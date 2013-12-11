8033853e17ca85591c3b424d45c36d1244ae7836
David Feeney <dfeeney@gmail.com>
2013-12-11 15:08:07 -0500

    correct barchart icon class

--- viewshare/apps/exhibit/static/freemix/js/layout/views/barchart.js
+++ viewshare/apps/exhibit/static/freemix/js/layout/views/barchart.js
@@ -6,7 +6,7 @@ define(["jquery",
 
     View.prototype.label = "Bar Chart";
     View.prototype.thumbnail = "/static/freemix/img/piechart-icon.png";
-    View.prototype.icon_class = "fa fa-barchart-o fa-3x";
+    View.prototype.icon_class = "fa fa-bar-chart-o fa-3x";
 
     View.prototype.viewClass = BarChartView;
     View.prototype.template_name = "barchart-view-template";
