767d3a0fb3fad77f383e13de839af8746acdb2c7
David Feeney <dfeeney@gmail.com>
2014-01-14 04:03:59 -0500

    eliminate use of handler500 with context

--- viewshare/apps/upload/migrations/0001_initial.py
+++ viewshare/apps/upload/migrations/0001_initial.py
@@ -5,7 +5,9 @@ from south.v2 import SchemaMigration
 from django.db import models
 
 class Migration(SchemaMigration):
-
+    depends_on=(
+        ('dataset', '0001_initial_models'),
+    )
     def forwards(self, orm):
         
         # Adding model 'URLDataSource'
