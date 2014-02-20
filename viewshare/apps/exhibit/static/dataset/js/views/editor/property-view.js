d3c6b15add6db1e85c5c11e7d757d372de3391aa
Bill Amberg <bamb@loc.gov>
2014-02-20 16:57:12 -0500

    Change editor label inputs based on 'input' event delay. Remove 'Save' and 'Cancel' buttons from the editor.

--- viewshare/apps/exhibit/static/dataset/js/views/editor/property-view.js
+++ viewshare/apps/exhibit/static/dataset/js/views/editor/property-view.js
@@ -27,6 +27,7 @@ define([
             this.model = options.model;
             this.$el = options.$el;
             this.deleteModalView = undefined;
+            this.timeoutId = undefined;
             // subscribe to model events
             this.model.Observer('loadDataSuccess').subscribe(
                 this.render.bind(this)
@@ -52,8 +53,11 @@ define([
 
         /** Event handler when a .name input is changed */
         changeLabelHandler: function(event) {
-            this.model.label = event.target.value;
-            return this.model.updateProperty();
+            clearTimeout(this.timeoutId);
+            this.timeoutId = setTimeout(function() {
+                this.model.label = event.target.value;
+                return this.model.updateProperty();
+            }.bind(this), 600);
         },
 
         /** Event handler when a .types input is changed */
@@ -118,7 +122,7 @@ define([
             }));
             // bind to DOM events
             this.$el.find('.name input').on(
-                'change', this.changeLabelHandler.bind(this));
+                'input', this.changeLabelHandler.bind(this));
             this.$el.find('.types select').on(
                 'change', this.changeTypeHandler.bind(this));
             this.$el.find('#delete-' + this.model.id()).on(
