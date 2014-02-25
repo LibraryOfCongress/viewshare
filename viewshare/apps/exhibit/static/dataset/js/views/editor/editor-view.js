3448d61cd5fe4efe31799ae7d92992f9ad15a268
Bill Amberg <bamb@loc.gov>
2014-02-25 14:21:59 -0500

    Set displayed property value to currentItemIndex when adding a property in the editor.

--- viewshare/apps/exhibit/static/dataset/js/views/editor/editor-view.js
+++ viewshare/apps/exhibit/static/dataset/js/views/editor/editor-view.js
@@ -92,6 +92,10 @@ define([
                 model: newPropertyModel,
                 $el: newPropertyEl
             });
+            if (this.model.properties.length > 0) {
+                newProperty.currentItemIndex = this.model
+                    .properties[0].currentItemIndex;
+            }
             this.propertyViews.push(newProperty);
             newPropertyEl.css( 'display', 'none' );
             if (propertyTr == null) {
