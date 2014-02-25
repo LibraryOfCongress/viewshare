5341dd4f050f9820e8c21845574ac0f68e021df1
Ryan Lee <ryanlee@zepheira.com>
2014-02-25 14:39:59 -0800

    replace jQuery-based browser detectiong with actual deprecated jQuery code for quick fix in 1.7 -> 1.10 transition

--- viewshare/static/viewshare/js/compatible.js
+++ viewshare/static/viewshare/js/compatible.js
@@ -4,17 +4,28 @@
 var Compatible = {};
 
 Compatible.checkBrowser = function() {
+    // Variables cribbed from jQuery 1.3.  Should be replaced by feature
+    // detection as user agent data is unreliable.
+    var userAgent = navigator.userAgent.toLowerCase();
+    var browser = {
+	version: (userAgent.match( /.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/ ) || [0,'0'])[1],
+	safari: /webkit/.test( userAgent ),
+	opera: /opera/.test( userAgent ),
+	msie: /msie/.test( userAgent ) && !/opera/.test( userAgent ),
+	mozilla: /mozilla/.test( userAgent ) && !/(compatible|webkit)/.test( userAgent )
+    };
+    
     var compatible = false;
-    if ($.browser.mozilla) {
-	if (parseFloat($.browser.version) >= 1.8) {
+    if (browser.mozilla) {
+	if (parseFloat(browser.version) >= 1.8) {
 	    compatible = true;
 	}
-    } else if ($.browser.msie) {
-	if (parseInt($.browser.version) >= 7) {
+    } else if (browser.msie) {
+	if (parseInt(browser.version) >= 7) {
 	    compatible = true;
 	}
-    } else if ($.browser.safari) {
-	if (parseInt($.browser.version) >= 300) {
+    } else if (browser.safari) {
+	if (parseInt(browser.version) >= 300) {
 	    compatible = true;
 	}
     }
