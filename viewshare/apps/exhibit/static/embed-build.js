30c30c05310daab0bed0ba13d5ea8ecfcfad87d8
David Feeney <dfeeney@gmail.com>
2014-03-06 18:31:20 -0500

    try embedding with  the hardcoded config external to the app main

--- viewshare/apps/exhibit/static/embed-build.js
+++ viewshare/apps/exhibit/static/embed-build.js
@@ -1,10 +1,90 @@
 {
-  "mainConfigFile": "embed-main.js",
-  "preserveLicenseComments": false,
-  "wrap": false,
-  "optimize": "uglify2",
-  "generateSourceMaps": false,
-  "findNestedDependencies": true,
-  "optimizeAllPluginResources": true
+//    "mainConfigFile": "embed-main.js",
+    "preserveLicenseComments": false,
+    "wrap": false,
+    "optimize": "uglify2",
+    "generateSourceMaps": false,
+    "findNestedDependencies": true,
+    "optimizeAllPluginResources": true,
+    "config": {
+        "exhibit": {
+            "prefix": "http://localhost:8500/simile/exhibit",
+            "bundle": true,
+            "autoCreate": false
+        },
+        "timeline": {
+            "prefix": "http://localhost:8500/simile/timeline/",
+            "ajax": "http://viewshare.org/static/simile/ajax/",
+            "bundle": true
+        },
+        "ajax": {
+            "history": false
+        },
+        "ext/time/time-extension": {
+            "bundle": true,
+            "prefix": "http://localhost:8500/simile/exhibit/extensions/time/"
+        },
+        "ext/map/map-extension": {
+            "bundle": true,
+            "prefix": "http://localhost:8500/simile/exhibit/extensions/map/"
+        },
+        "ext/openlayers/openlayers-extension": {
+            "bundle": true,
+            "prefix": "http://localhost:8500/simile/exhibit/extensions/openlayers/"
+        },
+        "ext/flot/flot-extension": {
+            "bundle": true,
+            "prefix": "http://localhost:8500/simile/exhibit/extensions/flot/"
+        }
+    },
+       "deps": ["nls/de/locale","nls/es/locale","nls/fr/locale","nls/locale","nls/nl/locale","nls/no/locale","nls/pt-br/locale","nls/root/locale","nls/sv/locale","ext/flot/nls/locale","ext/flot/nls/root/locale","ext/map/nls/de/locale","ext/map/nls/es/locale","ext/map/nls/fr/locale","ext/map/nls/locale","ext/map/nls/nl/locale","ext/map/nls/root/locale","ext/map/nls/sv/locale","ext/time/nls/de/locale","ext/time/nls/es/locale","ext/time/nls/fr/locale","ext/time/nls/locale","ext/time/nls/nl/locale","ext/time/nls/root/locale","ext/time/nls/sv/locale"],
+    "paths": {
+        "i18n": "simile/exhibit/lib/i18n",
+        "async": "simile/exhibit/lib/async",
+        "lib": "simile/exhibit/lib",
+        "nls": "simile/exhibit/nls",
+        "ext": "simile/exhibit/extensions",
+        "scripts": "simile/exhibit/scripts",
+        "exhibit": "simile/exhibit/exhibit",
+        "timeline": "simile/exhibit/ext/timeline/api/timeline-bundle",
+        "simile-ajax": "simile/exhibit/ext/ajax/api/simile-ajax-bundle",
+        "jquery": "freemix/js/lib/jquery",
+        "creole": "freemix/js/lib/creole",
+        "openlayers": "simile/exhibit/extensions/openlayers/lib/OpenLayers",
+        "text": 'freemix/js/lib/text',
+        "display": "freemix/js/display"
+    },
+    "map": {
+        "*": {
+            "lib/jquery": "jquery"
+        }
+    },
+    "shim": {
+        "jquery": {
+            "exports": "jQuery"
+        },
+        "creole": {
+            "exports": "Parse"
+        },
+        "openlayers": {
+            "exports": "OpenLayers"
+        },
+        "lib/jquery": {
+            "exports": "jQuery"
+        },
+        "lib/base64": {
+            "exports": "Base64"
+        },
+        "lib/jquery.history": {
+            "deps": ["lib/jquery"],
+            "exports": "History"
+        },
+        "lib/jquery.history.shim": {
+            "deps": ["lib/jquery.history"]
+        },
+        "lib/jquery.nouislider": {
+            "deps": ["lib/jquery"]
+        }
+    }
 
 }
--- viewshare/apps/exhibit/static/embed-main.js
+++ viewshare/apps/exhibit/static/embed-main.js
@@ -1,86 +1,86 @@
 
-requirejs.config({
+requirejs.config(requireconfig || {
 //    "baseUrl": "/static/",
-    "config": {
-        "exhibit": {
-            "prefix": "http://viewshare.org/static/simile/exhibit",
-            "bundle": true,
-            "autoCreate": false
-        },
-        "timeline": {
-            "prefix": "http://viewshare.org/static/simile/timeline/",
-            "ajax": "http://viewshare.org/static/simile/ajax/",
-            "bundle": true
-        },
-        "ajax": {
-            "history": false
-        },
-        "ext/time/time-extension": {
-            "bundle": true,
-            "prefix": "http://viewshare.org/static/simile/exhibit/extensions/time/"
-        },
-        "ext/map/map-extension": {
-            "bundle": true,
-            "prefix": "http://viewshare.org/static/simile/exhibit/extensions/map/"
-        },
-        "ext/openlayers/openlayers-extension": {
-            "bundle": true,
-            "prefix": "http://viewshare.org/static/simile/exhibit/extensions/openlayers/"
-        },
-        "ext/flot/flot-extension": {
-            "bundle": true,
-            "prefix": "http://viewshare.org/static/simile/exhibit/extensions/flot/"
-        }
-    },
-    "deps": ["nls/de/locale","nls/es/locale","nls/fr/locale","nls/locale","nls/nl/locale","nls/no/locale","nls/pt-br/locale","nls/root/locale","nls/sv/locale","ext/flot/nls/locale","ext/flot/nls/root/locale","ext/map/nls/de/locale","ext/map/nls/es/locale","ext/map/nls/fr/locale","ext/map/nls/locale","ext/map/nls/nl/locale","ext/map/nls/root/locale","ext/map/nls/sv/locale","ext/time/nls/de/locale","ext/time/nls/es/locale","ext/time/nls/fr/locale","ext/time/nls/locale","ext/time/nls/nl/locale","ext/time/nls/root/locale","ext/time/nls/sv/locale"],
-    "paths": {
-        "i18n": "simile/exhibit/lib/i18n",
-        "async": "simile/exhibit/lib/async",
-        "lib": "simile/exhibit/lib",
-        "nls": "simile/exhibit/nls",
-        "ext": "simile/exhibit/extensions",
-        "scripts": "simile/exhibit/scripts",
-        "exhibit": "simile/exhibit/exhibit",
-        "timeline": "simile/exhibit/ext/timeline/api/timeline-bundle",
-        "simile-ajax": "simile/exhibit/ext/ajax/api/simile-ajax-bundle",
-        "jquery": "freemix/js/lib/jquery",
-        "creole": "freemix/js/lib/creole",
-        "openlayers": "simile/exhibit/extensions/openlayers/lib/OpenLayers",
-        "text": 'freemix/js/lib/text',
-        "display": "freemix/js/display"
-    },
-    "map": {
-        "*": {
-            "lib/jquery": "jquery"
-        }
-    },
-    "shim": {
-        "jquery": {
-            "exports": "jQuery"
-        },
-        "creole": {
-            "exports": "Parse"
-        },
-        "openlayers": {
-            "exports": "OpenLayers"
-        },
-        "lib/jquery": {
-            "exports": "jQuery"
-        },
-        "lib/base64": {
-            "exports": "Base64"
-        },
-        "lib/jquery.history": {
-            "deps": ["lib/jquery"],
-            "exports": "History"
-        },
-        "lib/jquery.history.shim": {
-            "deps": ["lib/jquery.history"]
-        },
-        "lib/jquery.nouislider": {
-            "deps": ["lib/jquery"]
-        }
-    }
+//    "config": {
+//        "exhibit": {
+//            "prefix": "http://viewshare.org/static/simile/exhibit",
+//            "bundle": true,
+//            "autoCreate": false
+//        },
+//        "timeline": {
+//            "prefix": "http://viewshare.org/static/simile/timeline/",
+//            "ajax": "http://viewshare.org/static/simile/ajax/",
+//            "bundle": true
+//        },
+//        "ajax": {
+//            "history": false
+//        },
+//        "ext/time/time-extension": {
+//            "bundle": true,
+//            "prefix": "http://viewshare.org/static/simile/exhibit/extensions/time/"
+//        },
+//        "ext/map/map-extension": {
+//            "bundle": true,
+//            "prefix": "http://viewshare.org/static/simile/exhibit/extensions/map/"
+//        },
+//        "ext/openlayers/openlayers-extension": {
+//            "bundle": true,
+//            "prefix": "http://viewshare.org/static/simile/exhibit/extensions/openlayers/"
+//        },
+//        "ext/flot/flot-extension": {
+//            "bundle": true,
+//            "prefix": "http://viewshare.org/static/simile/exhibit/extensions/flot/"
+//        }
+//    },
+//    "deps": ["nls/de/locale","nls/es/locale","nls/fr/locale","nls/locale","nls/nl/locale","nls/no/locale","nls/pt-br/locale","nls/root/locale","nls/sv/locale","ext/flot/nls/locale","ext/flot/nls/root/locale","ext/map/nls/de/locale","ext/map/nls/es/locale","ext/map/nls/fr/locale","ext/map/nls/locale","ext/map/nls/nl/locale","ext/map/nls/root/locale","ext/map/nls/sv/locale","ext/time/nls/de/locale","ext/time/nls/es/locale","ext/time/nls/fr/locale","ext/time/nls/locale","ext/time/nls/nl/locale","ext/time/nls/root/locale","ext/time/nls/sv/locale"],
+//    "paths": {
+//        "i18n": "simile/exhibit/lib/i18n",
+//        "async": "simile/exhibit/lib/async",
+//        "lib": "simile/exhibit/lib",
+//        "nls": "simile/exhibit/nls",
+//        "ext": "simile/exhibit/extensions",
+//        "scripts": "simile/exhibit/scripts",
+//        "exhibit": "simile/exhibit/exhibit",
+//        "timeline": "simile/exhibit/ext/timeline/api/timeline-bundle",
+//        "simile-ajax": "simile/exhibit/ext/ajax/api/simile-ajax-bundle",
+//        "jquery": "freemix/js/lib/jquery",
+//        "creole": "freemix/js/lib/creole",
+//        "openlayers": "simile/exhibit/extensions/openlayers/lib/OpenLayers",
+//        "text": 'freemix/js/lib/text',
+//        "display": "freemix/js/display"
+//    },
+//    "map": {
+//        "*": {
+//            "lib/jquery": "jquery"
+//        }
+//    },
+//    "shim": {
+//        "jquery": {
+//            "exports": "jQuery"
+//        },
+//        "creole": {
+//            "exports": "Parse"
+//        },
+//        "openlayers": {
+//            "exports": "OpenLayers"
+//        },
+//        "lib/jquery": {
+//            "exports": "jQuery"
+//        },
+//        "lib/base64": {
+//            "exports": "Base64"
+//        },
+//        "lib/jquery.history": {
+//            "deps": ["lib/jquery"],
+//            "exports": "History"
+//        },
+//        "lib/jquery.history.shim": {
+//            "deps": ["lib/jquery.history"]
+//        },
+//        "lib/jquery.nouislider": {
+//            "deps": ["lib/jquery"]
+//        }
+//    }
 });
 
 require([
@@ -107,40 +107,6 @@ require([
         "display/facets/logo",
         "display/facets/text"
     ], function($, initialize) {
-        var static_url = window.Freemix.staticUrl;
-        requirejs.config({
-                "config": {
-                    "exhibit": {
-                        "prefix": static_url + "simile/exhibit",
-                        "bundle": true,
-                        "autoCreate": false
-                    },
-                    "timeline": {
-                        "prefix": static_url + "simile/timeline/",
-                        "ajax": "http://viewshare.org/static/simile/ajax/",
-                        "bundle": true
-                    },
-                    "ajax": {
-                        "history": false
-                    },
-                    "ext/time/time-extension": {
-                        "bundle": true,
-                        "prefix": static_url + "simile/exhibit/extensions/time/"
-                    },
-                    "ext/map/map-extension": {
-                        "bundle": true,
-                        "prefix": static_url + "simile/exhibit/extensions/map/"
-                    },
-                    "ext/openlayers/openlayers-extension": {
-                        "bundle": true,
-                        "prefix": static_url + "simile/exhibit/extensions/openlayers/"
-                    },
-                    "ext/flot/flot-extension": {
-                        "bundle": true,
-                        "prefix": static_url + "simile/exhibit/extensions/flot/"
-                    }
-                }
-        });
         $.noConflict(true);
         $(document).trigger("scriptsLoaded.exhibit");
         var randTmpId = "freemix-embed-shell-" + Math.round(Math.random()*1000);
