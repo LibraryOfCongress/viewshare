6b78f67ca8a0f24675230ad226940db40c561e74
Ryan Lee <ryanlee@zepheira.com>
2014-04-28 16:40:13 -0700

    add back other property methods unrelated to multiselect

--- viewshare/apps/exhibit/static/freemix/js/layout/widget.js
+++ viewshare/apps/exhibit/static/freemix/js/layout/widget.js
@@ -12,7 +12,94 @@ function ($, Widget, Freemix, Multiselect) {
     }
 
     Widget.prototype._setupPropertySelect = function(config, template, selector, key, types, nullable, exclude_other_types) {
-        // do nothing, not pertinent
+        this._populatePropertySelect(selector, types, nullable);
+        this._setupSelectPropertyHandler(config, template, selector, key);
+    };
+
+    function sorter(a,b) {
+        if (a.getLabel() < b.getLabel()) return -1;
+        if (a.getLabel() > b.getLabel()) return 1;
+        return 0;
+    }
+
+    Widget.prototype._generatePropertyList = function(types) {
+        var filter = [];
+        var database = Freemix.exhibit.database;
+
+        var result = {
+            order: []
+        }
+        if ((types ||[]).length == 0) {
+            types = this.propertyTypes;
+        }
+
+        for (var inx = 0 ; inx < types.length ; inx++) {
+            var type = types[inx];
+            result[type] = database.getPropertiesWithTypes([type]);
+            if (result[type].length > 0) {
+                result[type].sort(sorter);
+                filter = filter.concat($.map(result[type], function(v,i) {
+                    return v.getID();
+                }));
+                result["order"].push(type);
+            }
+        }
+
+        var filtered = database.getPropertyObjects(database.getFilteredProperties(filter));
+        filtered.sort(sorter);
+        result["others"] = filtered;
+        return result;
+    };
+
+    Widget.prototype._propertyRenderer = function(prop) {
+        return prop.getID();
+    }
+
+    Widget.prototype._buildOptionGroup = function(label, properties) {
+        var optgroup = $("<optgroup>");
+        optgroup.attr("label", label);
+        for (var inx = 0 ; inx < properties.length ; inx++) {
+            var prop = properties[inx];
+            var option = $("<option>");
+            option.attr("value", this._propertyRenderer(prop));
+            option.text(prop.getLabel());
+            optgroup.append(option);
+        }
+        return optgroup;
+    }
+
+    Widget.prototype._populatePropertySelect = function(selector, types, nullable) {
+        var properties = this._generatePropertyList(types);
+        for (var inx = 0 ; inx < properties.order.length ; inx++) {
+            var type = properties.order[inx];
+            selector.append(this._buildOptionGroup(type + " properties", properties[type]));
+        }
+
+        if (properties.others.length > 0) {
+            selector.append(this._buildOptionGroup("other properties", properties.others));
+        }
+        if (nullable) {
+            selector.prepend("<option value=''></option>");
+        }
+    };
+
+    Widget.prototype._setupSelectPropertyHandler = function(config, template, selector, key) {
+        var widget = this;
+        selector.change(function() {
+            var value = $(this).val();
+            if (value && value !== ( "" || undefined)) {
+                config[key] = value;
+            } else {
+                config[key] = undefined;
+            }
+            widget.triggerChange(config, template);
+
+
+         }).val(config[key]);
+
+        if (!selector.val() && selector.get(0).options.length > 0) {
+           selector.get(0).options[0].selected = true;
+        }
     };
 
     Widget.prototype._setupPropertyMultiSelect = function(config, template, selector, key, default_all) {
@@ -64,9 +151,11 @@ function ($, Widget, Freemix, Multiselect) {
     };
 
     Widget.prototype.remove = function() {
-        this._multiselect.container.off('modify.multiselect');
-        this._multiselect.destroy();
-        this._multiselect = null;
+        if (typeof this._multiselect !== "undefined") {
+            this._multiselect.container.off('modify.multiselect');
+            this._multiselect.destroy();
+            this._multiselect = null;
+        }
         this.findWidget().remove();
     };
 
